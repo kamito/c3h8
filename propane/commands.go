1a1b4c9a3894afa40b0aa9536a76b2afcfdeb89d
Shinichirow KAMITO <shinichirow@kamito.net>
2015-07-27 10:28:28 +0900

    Fix command `new` with new document file.

--- propane/commands.go
+++ propane/commands.go
@@ -1,34 +1,48 @@
 package propane
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/codegangsta/cli"
 )
 
 var Commands = []cli.Command{
 	commandServer,
+	commandNew,
 }
 
 var commandServer = cli.Command{
 	Name:  "server",
-	Usage: "",
+	Usage: "c3h8 server [options]",
 	Description: `
 `,
 	Action: doServer,
-	Flags: []cli.Flag {
+	Flags: []cli.Flag{
 		cli.StringFlag{
-			Name: "bind-addr, a",
+			Name:  "bind-addr, a",
 			Value: "0.0.0.0",
 			Usage: "--bind-addr=0.0.0.0",
 		},
 		cli.IntFlag{
-			Name: "port, p",
+			Name:  "port, p",
 			Value: 8088,
 			Usage: "--port=8088",
 		},
 	},
+}
+
+var commandNew = cli.Command{
+	Name:  "new",
+	Usage: "c3h8 new FILENAME",
+	Description: `
+`,
+	Action: doNewFile,
+	Flags:  []cli.Flag{},
 }
 
 func debug(v ...interface{}) {
@@ -45,4 +59,28 @@ func assert(err error) {
 
 func doServer(c *cli.Context) {
 	RunServer(c)
+}
+
+func doNewFile(c *cli.Context) {
+	args := c.Args()
+	if len(args) > 0 {
+		filename := args[0]
+		now := time.Now()
+		newfilename := now.Format("20060102150405") + "-" + filename + ".md"
+		newfilepath := filepath.Join(CurDir(), newfilename)
+		src := []byte("#\n")
+		err := ioutil.WriteFile(newfilepath, src, 0644)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Created " + newfilename)
+	} else {
+		fmt.Println(`Please input FILENAME
+
+USAGE:
+
+    $ c3h8 new new-file
+
+`)
+	}
 }
